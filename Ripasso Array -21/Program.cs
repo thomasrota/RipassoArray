2bfebefe13d33077de1e640746d4a6e0cb8d5291
rota.20196 <rota.20196@LAB2-13PC17>
2023-01-13 12:20:34 +0100

    Aggiungere i file di progetto.

---
+++ Ripasso Array -21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ripasso_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
